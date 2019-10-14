7d9a61b466e02c03722791c9ede9051c8cc587b8
pete842 <pierrekohler94@gmail.com>
2019-10-14 16:41:41 +0200

    add script for db creation

---
+++ db/create_libmovie.sql
@@ -0,0 +1,41 @@
+drop database libmovie;
+create database if not exists libmovie;
+use libmovie;
+
+create table if not exists medias
+	(
+		`id` int unsigned not null auto_increment,
+        `title` varchar(250) not null,
+        `release` timestamp,
+        `duration` int unsigned,
+        `main_genre` varchar(250),
+        `rating` tinyint unsigned,
+        primary key (`id`)
+	);
+    
+create table if not exists users
+	(
+		`id` int unsigned not null auto_increment,
+        `username` varchar(250) not null,
+        `firstname` varchar(250) not null,
+        `lastname` varchar(250) not null,
+        `email` varchar(250) not null,
+        `member_since` timestamp not null default current_timestamp,
+        primary key (`id`)
+	);
+    
+create table if not exists media_user
+	(
+		`id` int unsigned not null auto_increment,
+        `user_id` int unsigned not null,
+        `media_id` int unsigned not null ,
+        `rating` tinyint unsigned,
+        `watched` timestamp null default null,
+        primary key (`id`),
+		foreign key (`user_id`) references `users`(`id`),
+		foreign key (`media_id`) references `medias`(`id`)
+	);
+
+
+        
+        
