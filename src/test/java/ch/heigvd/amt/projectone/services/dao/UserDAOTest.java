6a50b16a2779330de52e5e08e6a615a34b77c9c4
pete842 <pierrekohler94@gmail.com>
2019-11-06 18:43:27 +0100

    test arqu give up

--- src/test/java/ch/heigvd/amt/projectone/services/dao/UserDAOTest.java
+++ src/test/java/ch/heigvd/amt/projectone/services/dao/UserDAOTest.java
@@ -1,15 +1,58 @@
 package ch.heigvd.amt.projectone.services.dao;
 
-import org.junit.jupiter.api.Test;
+import ch.heigvd.amt.projectone.model.entities.User;
+import org.arquillian.container.chameleon.deployment.api.DeploymentParameters;
+import org.arquillian.container.chameleon.deployment.maven.MavenBuild;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
+import java.sql.SQLException;
 
-class UserDAOTest {
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+@MavenBuild
+@DeploymentParameters(testable = true)
+public class UserDAOTest {
 
     @EJB
-    UserDAOLocal userDAO;
+    UserDAO usersDao;
+
+
+    /*@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(UserDAO.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }*/
+
+
 
     @Test
-    void itShouldBePossibleToCreateUser() {
+    public void findAll() throws SQLException {
+        User john = User.builder()
+                .username("jdoe")
+                .firstName("John")
+                .lastName("Doe")
+                .email("john@doe.org")
+                .password("secret").build();
+
+        User johnCreated = usersDao.create(john);
+        User johnLoaded = usersDao.getUserByUsername(john.getUsername());
+
+        assertEquals(john, johnCreated);
+        assertEquals(john, johnLoaded);
+        assertSame(john, johnCreated);
+        assertNotSame(john, johnLoaded);
+    }
+
+    @Test
+    public void getUserByUsername() {
+    }
+
+    @Test
+    public void create() {
     }
 }
